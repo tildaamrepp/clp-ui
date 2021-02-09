50d8b8a6ef97d596377f564de0ba092b76f66525
Tilda Amre <tilda.amre@frontwalker.se>
2021-02-09 10:14:15 +0100

    locale

--- src/theme/dark.js
+++ src/theme/dark.js
@@ -4,41 +4,45 @@ import green from "./colors/green";
 import blue from "./colors/blue";
 import imageFile from "../stories/assets/Cloudplanning-inverterad.png";
 import { yellow } from "@material-ui/core/colors";
+import { svSE } from "@material-ui/core/locale";
 const image = {
   src: imageFile,
   alt: "clp-logo",
 };
-const theme = createMuiTheme({
-  logo: image,
-  palette: {
-    type: "dark",
-    primary: cyan,
-    secondary: green,
-    warning: yellow,
-    background: {
-      default: "#161C2D",
-      paper: "#2E3547",
-    },
-  },
-  typography: {
-    fontFamily: ["Poppins", "sans-serif"].join(","),
-  },
-  overrides: {
-    MuiButton: {
-      root: {
-        textTransform: "none",
-        fontWeight: "400",
-        borderRadius: "0.6rem",
+const theme = createMuiTheme(
+  {
+    logo: image,
+    palette: {
+      type: "dark",
+      primary: cyan,
+      secondary: green,
+      warning: yellow,
+      background: {
+        default: "#161C2D",
+        paper: "#2E3547",
       },
     },
-    MuiAppBar: {
-      colorPrimary: {
-        backgroundColor: blue[900],
-        "& .MuiTabs-root": {
-          backgroundColor: blue[700],
+    typography: {
+      fontFamily: ["Poppins", "sans-serif"].join(","),
+    },
+    overrides: {
+      MuiButton: {
+        root: {
+          textTransform: "none",
+          fontWeight: "400",
+          borderRadius: "0.6rem",
+        },
+      },
+      MuiAppBar: {
+        colorPrimary: {
+          backgroundColor: blue[900],
+          "& .MuiTabs-root": {
+            backgroundColor: blue[700],
+          },
         },
       },
     },
   },
-});
+  svSE
+);
 export default theme;
--- src/theme/light.js
+++ src/theme/light.js
@@ -4,95 +4,99 @@ import green from "./colors/green";
 import blue from "./colors/blue";
 import imageFile from "../stories/assets/Cloudplanning.svg";
 import { yellow } from "@material-ui/core/colors";
+import { svSE } from "@material-ui/core/locale";
 const image = {
   src: imageFile,
   alt: "clp-logo",
 };
 const defaultTheme = createMuiTheme();
 
-const theme = createMuiTheme({
-  logo: image,
-  palette: {
-    type: "light",
-    primary: cyan,
-    secondary: green,
-    warning: yellow,
-    background: {
-      default: "#E5E5E5",
-      paper: "#ffffff",
-    },
-  },
-  appBar: {
-    color: green,
-  },
-
-  typography: {
-    fontFamily: ["Poppins", "sans-serif"].join(","),
-  },
-  tabs: {
-    backgroundColor: blue[700],
-  },
-  overrides: {
-    MuiButton: {
-      root: {
-        textTransform: "none",
-        fontWeight: "400",
-        borderRadius: "0.6rem",
+const theme = createMuiTheme(
+  {
+    logo: image,
+    palette: {
+      type: "light",
+      primary: cyan,
+      secondary: green,
+      warning: yellow,
+      background: {
+        default: "#E5E5E5",
+        paper: "#ffffff",
       },
     },
-    MuiTab: {
-      root: {
-        textTransform: "none",
-        [defaultTheme.breakpoints.up("md")]: {
-          minWidth: "115px",
+    appBar: {
+      color: green,
+    },
+
+    typography: {
+      fontFamily: ["Poppins", "sans-serif"].join(","),
+    },
+    tabs: {
+      backgroundColor: blue[700],
+    },
+    overrides: {
+      MuiButton: {
+        root: {
+          textTransform: "none",
+          fontWeight: "400",
+          borderRadius: "0.6rem",
         },
       },
-    },
-    MuiAppBar: {
-      colorPrimary: {
-        backgroundColor: blue[700],
+      MuiTab: {
+        root: {
+          textTransform: "none",
+          [defaultTheme.breakpoints.up("md")]: {
+            minWidth: "115px",
+          },
+        },
       },
-    },
-    MuiDrawer: {},
-    MuiListItem: {
-      root: {
-        "&$selected, &$selected:hover": {
-          backgroundColor: cyan[100],
-          color: cyan[500],
-          "&$svg": {
+      MuiAppBar: {
+        colorPrimary: {
+          backgroundColor: blue[700],
+        },
+      },
+      MuiDrawer: {},
+      MuiListItem: {
+        root: {
+          "&$selected, &$selected:hover": {
+            backgroundColor: cyan[100],
+            color: cyan[500],
+            "&$svg": {
+              backgroundColor: cyan[100],
+              color: cyan[500],
+            },
+          },
+          "&$selected:hover": {
             backgroundColor: cyan[100],
             color: cyan[500],
           },
-        },
-        "&$selected:hover": {
-          backgroundColor: cyan[100],
-          color: cyan[500],
-        },
-        "&:hover": {
-          backgroundColor: "white",
-          color: cyan[500],
+          "&:hover": {
+            backgroundColor: "white",
+            color: cyan[500],
+          },
         },
       },
-    },
-    MuiTypography: {
-      h1: {
-        fontSize: "2.5rem",
-        fontWeight: "400",
-      },
-      h2: {
-        fontSize: "2rem",
-        fontWeight: "400",
-      },
-      h3: {
-        fontSize: "1.75rem",
-      },
-      h4: {
-        fontSize: "1.50rem",
-      },
-      h5: {
-        fontSize: "1.25rem",
+      MuiTypography: {
+        h1: {
+          fontSize: "2.5rem",
+          fontWeight: "400",
+        },
+        h2: {
+          fontSize: "2rem",
+          fontWeight: "400",
+        },
+        h3: {
+          fontSize: "1.75rem",
+        },
+        h4: {
+          fontSize: "1.50rem",
+        },
+        h5: {
+          fontSize: "1.25rem",
+        },
       },
     },
   },
-});
+  svSE
+);
 export default theme;
